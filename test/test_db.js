81c16de6c62be3769d006cf70ba7fdfef15910b4
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-01 11:03:19 +0800

    nothing cool updated

---
+++ test/test_db.js
@@ -0,0 +1,15 @@
+/**
+ *  Test cases
+ *  ==========
+ *
+ *  1. System booting: XML data transformed into JSON in database
+ *  2. News category updates
+ */
+
+const assert = require('assert')
+
+describe('Bootstrap', _ => {
+  it('should return true', _ => {
+    assert.equal(1, 1)
+  })
+})
